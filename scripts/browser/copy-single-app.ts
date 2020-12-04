f0621b9cd3e017e4644c0a12c70644e8ecd67af9
shintaroNagata <cybozu.nagata@gmail.com>
2020-12-05 00:08:47 +0900

    refactor: write the type definition of kintone object

--- scripts/browser/copy-single-app.ts
+++ scripts/browser/copy-single-app.ts
@@ -2,25 +2,27 @@ import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 import { copySingleApp } from "../../src";
 
 kintone.events.on("app.record.index.show", (event: unknown) => {
-  const headerElement: HTMLElement = kintone.app.getHeaderSpaceElement();
-  const button = document.createElement("button");
-  button.innerText = "copy!";
-  const client = new KintoneRestAPIClient();
-  button.addEventListener(
-    "click",
-    async () => {
-      button.innerText = "...";
-      await copySingleApp({
-        from: {
-          client,
-          appId: kintone.app.getId(),
-        },
-        to: { client },
-      });
-      button.innerText = "Done!";
-      button.disabled = true;
-    },
-    { once: true }
-  );
-  headerElement.appendChild(button);
+  const headerSpaceElement = kintone.app.getHeaderSpaceElement();
+  if (headerSpaceElement) {
+    const button = document.createElement("button");
+    button.innerText = "Copy!";
+    const client = new KintoneRestAPIClient();
+    button.addEventListener(
+      "click",
+      async () => {
+        button.innerText = "...";
+        await copySingleApp({
+          from: {
+            client,
+            appId: String(kintone.app.getId()),
+          },
+          to: { client },
+        });
+        button.innerText = "Done!";
+        button.disabled = true;
+      },
+      { once: true }
+    );
+    headerSpaceElement.appendChild(button);
+  }
 });
--- types/global/kintone.d.ts
+++ types/global/kintone.d.ts
@@ -1,1 +1,12 @@
-declare const kintone: any;
+declare const kintone: {
+  app: {
+    getId(): number | null;
+    getHeaderSpaceElement(): HTMLElement | null;
+  };
+  events: {
+    on: (
+      eventType: string | string[],
+      callback: (event: unknown) => unknown
+    ) => void;
+  };
+};
